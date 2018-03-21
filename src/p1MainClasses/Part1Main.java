1a84e6c80ef548978db26ee2f458432a1ea9c9ec
JavierO <javier.ortiz13@upr.edu>
2018-03-20 23:57:39 -0400

    added partial implementation of toSetArray Method
    also added important TODO's

--- src/p1MainClasses/Part1Main.java
+++ src/p1MainClasses/Part1Main.java
@@ -1,11 +1,12 @@
 package p1MainClasses;
 import java.io.FileNotFoundException;
+
+import dataGenerator.DataReader;
 import interfaces.MySet;
 
 /**
  * 
  * @author Javier A. Ortiz García 802-16-4820
- * @param <E>
  *
  */
 
@@ -20,13 +21,55 @@ public class Part1Main {
 //		Apply the selected strategy (or each one of the four strategies) to construct the final intersection set  that results (or sets that result) from the particular input used; and then show results. 
 
 		
+		//TODO: check if necessary. perhaps files are generated beforehand
+		try {
+			FilesGeneratorMain.main(new String[0]);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		//read input from args
+		int arg = -1;
+		
+		if (args.length <= 1) {
+			if (args.length == 1) 
+				arg = Integer.parseInt(args[0]); 
+		} 
+		else 
+			System.out.println("Invalid number of parameters. Must be <= 1.");
+		
+		switch(arg) {
+		case 1:
+			//print: Final Set by P1
+			break;
+			
+		case 2:
+			//print: Final Set by P2
+			break;
+			
+		case 3:
+			//print: Final Set by P3
+			break;
+			
+		case 4:
+			//print: Final Set by P4
+			break;
+
+		default:
+			//print: Final Set by all P's
+			break;
+		}
 	}
 
 	
 	private static MySet<Integer>[] toSetArray() throws FileNotFoundException{
 		
+		DataReader reader = new DataReader();
 		
+		Integer[][][] data = (Integer[][][]) reader.readDataFiles();
+		
+		//TODO: change from Integer array into MySet object
 		return null;
 	}
 	
